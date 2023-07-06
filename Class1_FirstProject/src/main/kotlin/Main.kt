2c65e413b75a064bb7d8cd5bd575bb04a5b52e0c
George <100698568+George-P-1@users.noreply.github.com>
2023-07-06 13:50:10 +0200

    Class1 - Introduction/Basics
    Variable types, printing

---
+++ Class1_FirstProject/src/main/kotlin/Main.kt
@@ -0,0 +1,45 @@
+fun main() {
+
+    // Part 1 (Intro and Concatenation)--------------------------------------------
+//    println("Hello World!")
+//
+//    var test = "before\n" // Variable
+//
+//    test = "after\n"
+//
+//    val pi = 3.14 // Constant
+//
+//    println(test + pi)
+//
+//    println("Pi rounded to 2 decimals, pi=$pi.")
+
+    // Part 2 (String Formatter) --------------------------------------------
+
+//    var radius = 2
+//
+//    println("You have ${2*pi*radius} circumference")
+
+//    var total = 100
+//    var deleted = 10
+//    println("Total Photos                 $total")
+//    println("Deleted Photos               $deleted")
+//    println("Current Number of  Photos    ${total-deleted}")
+
+    // Part 3 (Type assignment) --------------------------------------------
+//    val trip1 : Double = 3.2
+//    val trip2 : Double = 4.1
+//    val trip3 : Double = 1.72
+//    val TotalTripLength : Double = trip1+trip2+trip3
+//    println("$TotalTripLength miles to destination")
+
+    // Part 4 (String Concatenation)--------------------------------------------
+    var str1 = "String1"
+    var str2 = " String2"
+    println(str1 + str2 + " Combined both")
+    println("$str1$str2 Combined both")
+
+    // Part 5 (Boolean Operator)--------------------------------------------
+    val enableBool : Boolean = false
+    println("Enable status: $enableBool")
+
+}
